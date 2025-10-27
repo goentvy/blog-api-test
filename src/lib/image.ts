845277a19d988f92d65bfcf66a9b668b836261ed
goentvy <entvyv@gmail.com>
2025-10-27 10:27:15 +0900

    thumbnails image add

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,7 +1,8 @@
-const IMAGE_BASE_URL = 'https://entvy-blog.onrender.com/api/image'
+// const IMAGE_BASE_URL = 'https://entvy-blog.onrender.com/api/image'
 
 export const resolveImageUrl = (path: string) => {
-    if(!path) return ''
-    const filename = path.split('/').pop() // 파일명만 추출
-    return `${IMAGE_BASE_URL}/${filename}`
+  if (!path) return ''
+  const filename = path.split('/').pop()
+  const basePath = import.meta.env.DEV ? '' : '/blog-api-test'
+  return `${basePath}/assets/thumbnails/${filename}`
 }
